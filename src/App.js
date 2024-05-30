05f9b2e7182fd11808334b3948724ff2cc20ccdc
Sumit Dhakad <sumitdhakad2232@gmail.com>
2024-05-30 22:05:45 +0530

    Added smartlock

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import smartlookClient from 'smartlook-client';
 function App() {
 
 	useEffect(() => {
-		smartlookClient.init('274c0819d2e0ed0caaf2b06d93cbf2f3591f397e');
+		if (window.location.protocol === 'https:') {
+			smartlookClient.init('274c0819d2e0ed0caaf2b06d93cbf2f3591f397e');
+			console.log("someasdlfkjalksjdf");
+		}
 	}, [])
 
 	return (
